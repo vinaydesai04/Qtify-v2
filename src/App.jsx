a1ecb1f0626303ca39b544c1072c8f28eb456053
vinaydesai04 <vinaymd2002@gmail.com>
2026-04-30 18:08:12 +0530

    Permanently remove node_modules from GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 // src/App.jsx
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Section from "./components/Section/Section";
 
 function App() {
-  //const [searchTerm, setSearchTerm] = useState("");
-
   const handleSearch = (query) => {
-    setSearchTerm(query);
     console.log("Search query:", query);
     // later: use this query to filter items or hit a search API
   };
